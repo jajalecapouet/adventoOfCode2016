4d0b9f3e72addbb8d1da920013393dc3e5ffec3a
Jaja <nicolas.jarose@gmail.com>
2023-08-15 20:05:29 +0200

    some days

--- day9/day9.cpp
+++ day9/day9.cpp
@@ -2,17 +2,21 @@
 #include <fstream>
 #include <string>
 
-long long recuMark(std::string::size_type start, std::string::size_type len, const std::string& line) {
+long long recuMark(std::string::size_type start, std::string::size_type len, const std::string& line, bool part1) {
 	long long res = 0;
 	long long times;
 	std::string::size_type sub;
+
 	for (std::string::size_type idx = start; idx < start + len; ++idx) {
 		if (line[idx] == '(') {
 			sub = atoi(&line[++idx]);
 			idx = line.find('x', idx + 1) + 1;
 			times = atoi(&line[idx]);
 			idx = line.find(')', idx + 1) + 1;
-			res += times * recuMark(idx, sub, line);
+			if (part1)
+				res += (times * sub);
+			else
+				res += times * recuMark(idx, sub, line, part1);
 			idx += sub - 1;
 		}
 		else
@@ -23,27 +27,12 @@ long long recuMark(std::string::size_type start, std::string::size_type len, con
 
 int main() {
 	std::ifstream in("./Input.txt");
-	if (in.fail()) {
-		return errno;
-	}
 	std::string line;
-	long long res = 0;
-	long long times;
-	std::string::size_type sub;
+
+	if (in.fail())
+		return errno;
 	getline(in, line);
-	for (std::string::size_type idx = 0; idx < line.size(); ++idx) {
-		if (line[idx] == '(') {
-			sub = atoi(&line[++idx]);
-			idx = line.find('x', idx + 1) + 1;
-			times = atoi(&line[idx]);
-			idx = line.find(')', idx + 1) + 1;
-			res += (times * sub);
-			idx += sub - 1;
-		}
-		else
-			++res;
-	}
-	std::cout << "result part 1 is " << res << std::endl;
-	std::cout << "result part 2 is " << recuMark(0, line.size(), line) << std::endl;
+	std::cout << "result part 1 is " << recuMark(0, line.size(), line, true) << std::endl;
+	std::cout << "result part 2 is " << recuMark(0, line.size(), line, false) << std::endl;
 	return 0;
 }
